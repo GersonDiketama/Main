b1cdf47bdbda760e356e51fa59f94eb4ca8c1853
GersonGmd <gersondiketama@yahoo.com>
2022-05-25 16:10:08 -0500

    added expplaination of string format

--- my_program.py
+++ my_program.py
@@ -4,6 +4,9 @@ def sentence_maker(phrase):
     interrogatives = ("how", "what", "why")
     capitalized = phrase.capitalize()
     if phrase.startswith(interrogatives):
+        # This will check if the string starts with "any interrogatives" if yes then it will ran the line below:
+        # The line below is a string format which mean that the entered string will go wherever the curly brackets are located. example: if user enters: How are you
+        # the string will return as => How are you? or {How are you}?
         return "{}?".format(capitalized)
     else:
         return "{}.".format(capitalized)
